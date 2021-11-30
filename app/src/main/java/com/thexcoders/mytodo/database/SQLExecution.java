90b3031892be84492ed4bf33782502db674682d5
Anass ABEA <annassabe@gmail.com>
2021-11-30 20:30:56 +0100

    sharable code

---
+++ app/src/main/java/com/thexcoders/mytodo/database/SQLExecution.java
@@ -0,0 +1,33 @@
+package com.thexcoders.mytodo.database;
+
+import android.app.Activity;
+
+public class SQLExecution {
+
+    private Activity activity;
+
+    public interface SQLWithReturn {
+        void execute();
+        void executeOnUI();
+    }
+
+    public SQLExecution( Activity activity){
+        this.activity = activity;
+    }
+
+    public void runWithResult(SQLWithReturn exec){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                exec.execute();
+                activity.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        exec.executeOnUI();
+                    }
+                });
+            }
+        }).start();
+    }
+
+}
